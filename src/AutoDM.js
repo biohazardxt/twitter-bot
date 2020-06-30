b6691f8153e3fbde0996706910e915ca7381717c
biohazardxt <33681995+biohazardxt@users.noreply.github.com>
2020-06-30 14:47:43 -0400

    Update AutoDM.js

--- src/AutoDM.js
+++ src/AutoDM.js
@@ -29,19 +29,8 @@ const SendMessage = user => {
     }, timeout);
   }
 };
-const GenerateMessage = name => {
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  const d = new Date();
-  const dayName = days[d.getDay()];
-  return `Hi ${name} Thanks for .... \n Happy ${dayName} 😊😊 `; // your message
+
+  return `Hey there ${name} saw you are a fellow supporter! We thought you might be interested in helping the Trump 2020 campaign while treating yourself to some patriotic products. 15% of all profits go directly to supporting the Trump 2020 campaign. https://bit.ly/2VAfqid using the coupon TRUMP-2020 will save you 10% too! God Bless, Kevin`; // your message
   // My message   return `Hi ${name} Thanks for being a part of my social media network. I'am the @PicsrushE founder,A new Online Image Editor completely with web technologies,I'm also a reactjs developer and medium blogger.\n Happy to discuss anytime 😊  \n Happy ${dayName} 😊😊 `;
 };
 
